4c066ee8c73df72d4d04092cfdc85a9cc39e5cad
Avedis Kiyici <kiyiciavedis@gmail.com>
2017-09-23 14:34:54 -0400

    Adds update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,36 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server')
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59c6a28b6f25a404fa7e7cd6')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then(result => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59c6a3b36f25a404fa7e7d94')
+  }, {
+    $set: {
+      name: 'Ave Kiy'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then(result => {
+    console.log(result);
+  });
+  ///db.close();
+});
